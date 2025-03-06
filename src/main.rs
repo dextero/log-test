c526b726e8c86e2a7fff77753a7d35328d4d781b
Marcin Radomski <dextero@google.com>
2025-03-06 15:26:44 +0000

    Fully delegate log overrides to platform

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
-    android_logger::init_once(android_logger::Config::default().with_tag("log_test"));
+    android_logger::init_once(android_logger::Config::default().with_tag("log_test").with_max_level(log::LevelFilter::Info));
+    // Without this, log crate will apply its own filter regardless of platform overrides
+    log::set_max_level(log::LevelFilter::Trace);
     log::trace!("trace");
     log::debug!("debug");
     log::info!("info");
